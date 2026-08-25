09021b0a74481cefed64b556dfba7d324374e186
Sandeep Nunna <sandeepnunna90@gmail.com>
2026-08-24 21:23:24 -0400

    Add: trapping-rain-water - submission-8

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Uses O(n) time, O(n) space as well
+        # The algo is that at every position (the index is of length 1 (the whole block)) we calculate the max height of the blocks on its left and right. 
+        maxl = 0
+        maxr = 0
+        maxLeft = []
+        maxRight = [] 
+        storage = []
+        
+
+        for i in range(len(height)):
+            maxl = max(maxl, height[i])
+            maxLeft.append(maxl)
+            
+        
+        for i in range(len(height)-1, -1, -1):
+            maxr = max(maxr, height[i])
+            maxRight.append(maxr)
+        
+        maxRight.reverse()
+
+        for i in range(len(height)):
+            # image left is 2 and right is 3. but the height at your positon 
+            # of h[i] = 0 -> you can store 2 units of water
+            # if h[i] = 1 -> you can only store 1 unit of water
+
+            # diff = min(maxLeft[i], maxRight[i]) - height[i]
+            # if diff < 0: 
+            #     water = 0
+            storage.append(max(0, (min(maxLeft[i], maxRight[i]) - height[i])))
+            
+        return sum(storage)
